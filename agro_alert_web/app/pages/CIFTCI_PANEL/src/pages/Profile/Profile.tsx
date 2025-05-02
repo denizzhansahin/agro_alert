034c4801ab410749fc3a8039962444e79c57dcb1
Denizhan Şahin <denizhansahin@yaani.com>
2025-05-03 00:21:24 +0300

    deniz

--- agro_alert_web/app/pages/CIFTCI_PANEL/src/pages/Profile/Profile.tsx
+++ agro_alert_web/app/pages/CIFTCI_PANEL/src/pages/Profile/Profile.tsx
@@ -331,7 +331,7 @@ const Profile = () => {
           
           <button className="inline-flex items-center px-4 py-2 bg-red-600 hover:bg-red-700 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-red-500 transition">
         <LogOut className="h-5 w-5 mr-2" />
-        <Link href="/login">
+        <Link href="/pages/login">
         Çıkış Yap
         </Link>
         
